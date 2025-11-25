849ef32500a5d7f7513309fda61e486c4b224aa6
William de O Silva <will.os913@gmail.com>
2025-11-25 18:19:25 -0300

    chore: first build of project

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    return True  # algo simples só pra validar execução
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_structure.py
@@ -0,0 +1,16 @@
+import os
+
+
+def test_project_structure():
+    expected_folders = [
+        "frontend",
+        "backend",
+        "assets",
+        "config",
+        "bin",
+        "core",
+        "temp",
+    ]
+
+    for folder in expected_folders:
+        assert os.path.isdir(folder), f"Pasta '{folder}' está faltando!"
